685f8de4088dd73d282edfd02ba8e5c32fb85c18
Dasha <dashukovy@gmail.com>
2024-01-27 09:36:49 +0200

    third commit

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,1 @@
-const rel=document.querySelector(".burMenu"),nav=document.querySelector(".menu");rel.onclick=function(){nav.classList.toggle("menuOpen"),rel.classList.toggle("burOpen")};
+const rel=document.querySelector(".burMenu"),nav=document.querySelector(".menu");rel.onclick=function(){nav.classList.toggle("menuOpen"),rel.classList.toggle("burOpen")};const preloader=document.querySelector(".preloader");window.onload=()=>{document.querySelector(".preloader").style.display="none"};
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,8 @@ rel.onclick = function () {
     nav.classList.toggle('menuOpen')
     rel.classList.toggle('burOpen')
 }
+
+const preloader = document.querySelector('.preloader')
+window.onload = () => {
+    document.querySelector(".preloader").style.display = "none";
+}
