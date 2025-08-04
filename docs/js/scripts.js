783e33d15653e6ebd5896bf3adb1339c82518761
watersidex <dashukovy@gmail.com>
2025-08-04 20:58:53 +0300

    commit

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,15 @@
-const rel=document.querySelector(".burMenu"),nav=document.querySelector(".menu");rel.onclick=function(){nav.classList.toggle("menuOpen"),rel.classList.toggle("burOpen")};const preloader=document.querySelector(".preloader");window.onload=()=>{document.querySelector(".preloader").style.display="none"};const but=document.querySelector(".butt"),inputform=document.querySelector(".contactForm");but.onclick=function(){inputform.classList.toggle("showForm")};
+const rel = document.querySelector(".burMenu"),
+    nav = document.querySelector(".menu");
+rel.onclick = function () {
+    nav.classList.toggle("menuOpen"), rel.classList.toggle("burOpen")
+};
+const preloader = document.querySelector(".preloader");
+window.onload = () => {
+    document.querySelector(".preloader").style.display = "none"
+};
+const but = document.querySelector(".butt"),
+    inputform = document.querySelector(".contactForm");
+but.onclick = function () {
+    event.preventDefault()
+    inputform.classList.toggle("showForm")
+};
